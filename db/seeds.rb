9fcda0276680d5a620d8252aeff50d0d220ecbb2
Vincent Masse <vincentmasse3@gmail.com>
2020-06-02 07:23:13 +0200

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  Item.create!(
+    title: Faker::Creature::Cat.name,
+    description: Faker::Creature::Cat.breed,
+    price: '%.2f' % rand(1.00..50.00).to_d,
+    image_url:
+      "https://placekitten.com/#{rand(100..400)}/#{rand(100..400)}"
+  )
+end
+
+puts "Seed generated."
