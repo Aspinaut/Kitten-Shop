adae53e1194f8d5c382d8bd83967be15d618ae52
Vincent Masse <vincentmasse3@gmail.com>
2020-06-02 11:30:59 +0200

    Add cart to db

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,7 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :image_url, presence: true
+  has_many :join_table_carts_items
+  has_many :carts,through: :join_table_carts_items
 
 end
