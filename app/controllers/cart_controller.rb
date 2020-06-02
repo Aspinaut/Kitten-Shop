ce9e0d87b0ed5187b1d9908b46e974f5a068968c
Vincent Masse <vincentmasse3@gmail.com>
2020-06-02 22:44:34 +0200

    Add Item to Cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,9 +1,6 @@
 class CartController < ApplicationController
 
   def edit
-    puts '$$$$$$$$$$$$$$$$$$$'
-    puts params[:format]
-    puts '$$$$$$$$$$$$$$$$$$$'
     JoinTableCartsItem.create(cart_id: current_user.cart.id, item_id: params[:format])
     redirect_to root_path
 
--- app/models/join_table_carts_item.rb
+++ app/models/join_table_carts_item.rb
@@ -1,4 +1,6 @@
 class JoinTableCartsItem < ApplicationRecord
   belongs_to :item
   belongs_to :cart
+  validates :item,
+    uniqueness: true
 end
