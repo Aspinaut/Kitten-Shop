4b00c76422baa67efa08d9ee0bf75140d5ca26e0
Vincent Masse <vincentmasse3@gmail.com>
2020-06-03 09:42:42 +0200

    Add to Cart : show + delete + price

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,10 +3,23 @@ class CartController < ApplicationController
   def edit
     JoinTableCartsItem.create(cart_id: current_user.cart.id, item_id: params[:format])
     redirect_to root_path
-
   end
 
   def show
+    @items = current_user.cart.items
+    bill
+  end
+
+  def destroy
+    current_user.cart.items.destroy(params[:format])
+    redirect_to cart_path
+  end
+
+  def bill
+    @total_price = 0
+    current_user.cart.items.each do |item|
+      @total_price = @total_price + item.price
+    end
   end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,6 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :join_table_carts_items
   has_many :items, through: :join_table_carts_items
+
+
 end
