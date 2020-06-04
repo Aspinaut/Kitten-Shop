8f3b84d5a0b508fd3016b85d28dc9dbcd2f7b3ae
loutix <loic.dubrulle@hotmail.fr>
2020-06-04 15:09:58 +0200

    ajout de stripe

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root 'item#index'
-  resources :item, :order
-  resources :cart, only: [:show, :edit, :destroy]
+
+  resources :item, :order, :cart
   resources :item, only: [:show] do
-    resources :pictures, only: [:create, :new]
+  resources :pictures, only: [:create, :new]
   end
 end
