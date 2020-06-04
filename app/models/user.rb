536b705c9b37929609ce7efb67658a96702326ca
Reivnaj <pelletierjanvier@gmail.com>
2020-06-04 14:48:04 +0200

    mailer set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_one :cart
   has_many :orders
-  after_create :create_cart, :set_admin_to_false
+
+  after_create :create_cart
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 
   private
-
-  def set_admin_to_false
-    self.update(is_admin: false)
-  end
 
   def create_cart
     @cart = Cart.create(user_id: self.id)
