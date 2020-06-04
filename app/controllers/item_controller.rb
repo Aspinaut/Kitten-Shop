7e1ecf94432d8c45126d40834840d36f13bfccc8
Vincent Masse <vincentmasse3@gmail.com>
2020-06-04 09:19:13 +0200

    edit item

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -14,4 +14,22 @@ class ItemController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+    @item.update(title: params[:item_title],description: params[:item_description],price: params[:item_price])
+    #redirect_to new_item_picture_path(@item)
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(title: params[:item_title],description: params[:item_description],price: params[:item_price])
+    redirect_to new_item_picture_path(@item)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
 end
